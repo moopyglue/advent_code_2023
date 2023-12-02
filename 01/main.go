7607d26340be0e74885acb2592476c3dacf1ffef
Simon Wall <moopyglue@gmail.com>
2023-12-02 11:34:41 +0000

    tidy up day 2

--- 01/main.go
+++ 01/main.go
@@ -1,3 +1,5 @@
+// AOC 2023 - day 1 - Trebuchet?!
+
 package main
 
 import (
@@ -6,82 +8,105 @@ import (
 	"os"
 )
 
-var debug = map[string]bool{ "info":false }
+// debug flags
+var debug = map[string]bool{"info": false}
 
-func calc(trans map[string]int)(result int) {
+// same calculation routine for both part1 and aprt2, just uses a different
+// translation map
+
+func calc(trans map[string]int) (result int) {
 
 	result = 0
 
 	for _, line := range getlines() {
 
-		pinfo( "====================================" )
-		pinfo( "start",line )
-		last:=len(line)
+		// for each line in the inpu file...
 
-		left:=-1
-		for i:=0 ; i<last ; i++ {
-			ll:=line[i:]
+		pinfo("====================================")
+		pinfo("start", line)
+		last := len(line)
+
+		// scan the left side of the line for first found match from trans map
+		left := -1
+		for i := 0; i < last; i++ {
+			ll := line[i:]
 			pinfo(ll)
-			for m,n := range trans {
-				if len(m) > len(ll) {continue}
-				pinfo(" ",m,":",n,":",ll[:len(m)]," ")
-				if m == ll[:len(m)]  {
-					left=n
+			for m, n := range trans {
+				if len(m) > len(ll) {
+					continue
+				}
+				pinfo(" ", m, ":", n, ":", ll[:len(m)], " ")
+				if m == ll[:len(m)] {
+					left = n
 					break
 				}
 			}
 			pinfo("")
-			if left >= 0  { break }
+			if left >= 0 {
+				break
+			}
 		}
 
-		right:=-1
-		for i:=last ; i>0 ; i-- {
-			ll:=line[:i]
+		// scan the right side of the line for first found match from trans map
+		right := -1
+		for i := last; i > 0; i-- {
+			ll := line[:i]
 			pinfo(ll)
-			for m,n := range trans {
-				if len(m) > len(ll) {continue}
-				pinfo(" ",m,":",n,":",ll[len(ll)-len(m):]," ")
-				if m == ll[len(ll)-len(m):]  {
-					right=n
+			for m, n := range trans {
+				if len(m) > len(ll) {
+					continue
+				}
+				pinfo(" ", m, ":", n, ":", ll[len(ll)-len(m):], " ")
+				if m == ll[len(ll)-len(m):] {
+					right = n
 					break
 				}
 			}
 			pinfo("")
-			if right >= 0  { break }
+			if right >= 0 {
+				break
+			}
 		}
-		result = result + (10*left) + right
-		pinfo( left,right,result )
+
+		// add found numbers to the result
+		result = result + (10 * left) + right
+		pinfo(left, right, result)
 
 	}
 	return
 
 }
 
-func part1()(result int) {
+func part1() (result int) {
 
+	// just single digit strings
 	part1_mapping := map[string]int{
-		"0":0, "1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9,
+		"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
 	}
-	result=calc(part1_mapping)
+	result = calc(part1_mapping)
 	return
 }
 
-func part2()(result int) {
+func part2() (result int) {
 
+	// more complex pattern matches adding in full text version of numbers
 	part2_mapping := map[string]int{
-		"zero":0, "one":1, "two":2, "three":3, "four":4,
-		"five":5, "six":6, "seven":7, "eight":8 , "nine":9,
-		"0":0, "1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9,
+		"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4,
+		"five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9,
+		"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
 	}
-	result=calc(part2_mapping)
+	result = calc(part2_mapping)
 	return
 }
 
+// returns input as eitrhegr from standard input or uses first
+// command line parameter for filename
 func getlines() (lines []string) {
 
 	args := os.Args[1:]
 
 	if len(args) > 0 {
+		// use filename provided
 		file, _ := os.Open(args[0])
 		reader := bufio.NewScanner(file)
 		for reader.Scan() {
@@ -89,6 +114,7 @@ func getlines() (lines []string) {
 		}
 		file.Close()
 	} else {
+		// use STDIN
 		pinfo("reading from STDIN")
 		reader := bufio.NewScanner(os.Stdin)
 		for reader.Scan() {
@@ -99,16 +125,17 @@ func getlines() (lines []string) {
 
 }
 
+// debug printing for INFO style lines
 func pinfo(params ...interface{}) {
-	if debug["info"] { fmt.Println(params) }
+	if debug["info"] {
+		fmt.Println(params)
+	}
 }
-
 
 func main() {
 	part1res := part1()
 	part2res := part2()
-	fmt.Println("part 1 =",part1res)
-	fmt.Println("part 2 =",part2res)
+	fmt.Println("part 1 =", part1res)
+	fmt.Println("part 2 =", part2res)
 	os.Exit(0)
 }
-
--- 02/main.go
+++ 02/main.go
@@ -2,47 +2,51 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
-	"strings"
-    "regexp"
-	"encoding/json"
+	"regexp"
 	"strconv"
+	"strings"
 )
 
-var debug = map[string]bool{ "info":false }
+// debug flags
+var debug = map[string]bool{"info": false}
 
-func part1()(result int) {
+// part1
+func part1() (result int) {
 
-	allcounts := make( map[string]int )
+	allcounts := make(map[string]int)
 	result = 0
 
-    re := regexp.MustCompile(" *[:,;] *")
+	re := regexp.MustCompile(" *[:,;] *")
 	for _, line := range getlines() {
 
 		// extrat data from line
-		split := re.Split(line,-1)
-		if len(split)<2 { continue }
+		split := re.Split(line, -1)
+		if len(split) < 2 {
+			continue
+		}
 
 		// process header line
 		if split[0] == "all" {
 			pinfo("header found  ")
-			for _,v := range split[1:] {
-				k := strings.Split(v," ")
-				allcounts[k[1]],_= strconv.Atoi(k[0])
+			for _, v := range split[1:] {
+				k := strings.Split(v, " ")
+				allcounts[k[1]], _ = strconv.Atoi(k[0])
 			}
-			bytearray,_ := json.Marshal(allcounts)
+			bytearray, _ := json.Marshal(allcounts)
 			pinfo(string(bytearray))
 			continue
 		}
 
 		// process data line
 		failed := false
-		for _,v := range split[1:] {
-			k := strings.Split(v," ")
-			if i,_ := strconv.Atoi(k[0]) ; allcounts[k[1]] < i {
-				pinfo("maxed",i,v)
-				failed=true
+		for _, v := range split[1:] {
+			k := strings.Split(v, " ")
+			if i, _ := strconv.Atoi(k[0]); allcounts[k[1]] < i {
+				pinfo("maxed", i, v)
+				failed = true
 				break
 			}
 		}
@@ -50,37 +54,42 @@ func part1()(result int) {
 			pinfo("failed")
 			continue
 		}
-		k := strings.Split(split[0]," ")
-		i,_ := strconv.Atoi(k[1])
-		result=result+i
-		pinfo(split[0],result)
+		k := strings.Split(split[0], " ")
+		i, _ := strconv.Atoi(k[1])
+		result = result + i
+		pinfo(split[0], result)
 
 	}
-	pinfo("part 1 result =",result)
+	pinfo("part 1 result =", result)
 	return
 
 }
 
-func part2()(result int) {
+// part 2
+func part2() (result int) {
 
 	pinfo("part2")
 	result = 0
 
-    re := regexp.MustCompile(" *[:,;] *")
+	re := regexp.MustCompile(" *[:,;] *")
 	for _, line := range getlines() {
 
 		// extrat data from line
-		split := re.Split(line,-1)
-		if len(split)<2 { continue }
-		if split[0] == "all" { continue }
+		split := re.Split(line, -1)
+		if len(split) < 2 {
+			continue
+		}
+		if split[0] == "all" {
+			continue
+		}
 		pinfo(line)
 
 		// process data line
-		maxs := make( map[string]int )
-		for _,v := range split[1:] {
-			k := strings.Split(v," ")
+		maxs := make(map[string]int)
+		for _, v := range split[1:] {
+			k := strings.Split(v, " ")
 			cube_color := k[1]
-			cube_count,_ := strconv.Atoi(k[0])
+			cube_count, _ := strconv.Atoi(k[0])
 			if _, ok := maxs[cube_color]; !ok {
 				maxs[cube_color] = cube_count
 			} else {
@@ -89,25 +98,28 @@ func part2()(result int) {
 				}
 			}
 		}
-		bytearray,_ := json.Marshal(maxs)
+		bytearray, _ := json.Marshal(maxs)
 		pinfo(string(bytearray))
-		force:=1
-		for _,v := range maxs {
-			force=force*v
+		force := 1
+		for _, v := range maxs {
+			force = force * v
 		}
-		pinfo("force=",force)
-		result=result+force
+		pinfo("force=", force)
+		result = result + force
 	}
 
-	pinfo("part 2 result =",result)
+	pinfo("part 2 result =", result)
 	return
 }
 
+// returns input as eitrhegr from standard input or uses first
+// command line parameter for filename
 func getlines() (lines []string) {
 
 	args := os.Args[1:]
 
 	if len(args) > 0 {
+		// use filename provided
 		file, _ := os.Open(args[0])
 		reader := bufio.NewScanner(file)
 		for reader.Scan() {
@@ -115,6 +127,7 @@ func getlines() (lines []string) {
 		}
 		file.Close()
 	} else {
+		// use STDIN
 		pinfo("reading from STDIN")
 		reader := bufio.NewScanner(os.Stdin)
 		for reader.Scan() {
@@ -125,15 +138,17 @@ func getlines() (lines []string) {
 
 }
 
+// debug printing for INFO style lines
 func pinfo(params ...interface{}) {
-	if debug["info"] { fmt.Println(params) }
+	if debug["info"] {
+		fmt.Println(params)
+	}
 }
 
 func main() {
 	part1res := part1()
 	part2res := part2()
-	fmt.Println("part 1 =",part1res)
-	fmt.Println("part 2 =",part2res)
+	fmt.Println("part 1 =", part1res)
+	fmt.Println("part 2 =", part2res)
 	os.Exit(0)
 }
-
