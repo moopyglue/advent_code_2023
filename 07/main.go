1cbd91511e4733bfbeaacc6a8de4e0e59ba18d7b
Simon Wall <moopyglue@gmail.com>
2023-12-08 02:05:16 +0000

    complete day 7

--- 07/main.go
+++ 07/main.go
@@ -1,4 +1,4 @@
-// AOC 2023 - day ? - ?
+// AOC 2023 - day 7 - Camel Cards
 
 package main
 
@@ -12,43 +12,39 @@ import (
 	"strings"
 )
 
-var debug = map[string]bool{"info": false}
+var debug = map[string]bool{"info": true}
 
 type hand struct {
-	cards  string
-	bet    int64
-	sorter string
+	cards string
+	bet   int64
+	sort1 string
+	sort2 string
 }
 
 func main() {
 
 	var data = getlines()
-
-	// PART 1
 	part1res := int64(0)
+	part2res := int64(0)
+
+	// generate a list of hands and store different sort values depending on
+	// part1 or part2 rules in use.
 	var hands = []hand{}
 	for _, s := range data {
 		k := strings.Split(s, " ")
-		hands = append(hands, hand{cards: k[0], bet: i64(k[1]), sorter: gen_sort_value_part1(k[0])})
+		hands = append(hands, hand{cards: k[0], bet: i64(k[1]), sort1: gen_sort_value_part1(k[0]), sort2: gen_sort_value_part2(k[0])})
 	}
-	sort.Slice(hands, func(i, j int) bool { return hands[i].sorter < hands[j].sorter })
+
+	// sort using PART 1 rules
+	sort.Slice(hands, func(i, j int) bool { return hands[i].sort1 < hands[j].sort1 })
 	for k, hand := range hands {
-		//pinfo(hand, (int64(k)+1)*hand.bet)
 		part1res += (int64(k) + 1) * hand.bet
 	}
 
-	fmt.Println("")
-
-	// PART 2
-	part2res := int64(0)
-	hands = []hand{}
-	for _, s := range data {
-		k := strings.Split(s, " ")
-		hands = append(hands, hand{cards: k[0], bet: i64(k[1]), sorter: gen_sort_value_part2(k[0])})
-	}
-	sort.Slice(hands, func(i, j int) bool { return hands[i].sorter < hands[j].sorter })
+	// sort using PART 2 rules
+	sort.Slice(hands, func(i, j int) bool { return hands[i].sort2 < hands[j].sort2 })
 	for k, hand := range hands {
-		pinfo(hand, (int64(k)+1)*hand.bet)
+		//pinfo(hand)
 		part2res += (int64(k) + 1) * hand.bet
 	}
 
@@ -56,82 +52,84 @@ func main() {
 	fmt.Println("part 2 =", part2res)
 }
 
+// generates a sortable hand version usinhg part 1 rules
 func gen_sort_value_part1(s string) (r string) {
-	r = ""
 
-	// calculate sprtabl string that represents the order of
-	// winning hand status e.g. full, house, 5 of  kind, etc...
-	m := map[string]int{}
-	for n := 0; n < len(s); n++ {
-		m[s[n:n+1]]++
+	// calculate hand type and add softwable version of hand
+	r = handtype(s) + s
+	r = translate(r, "TJQKA", "ABCDE")
+	return
+
+}
+
+// generates a sortable hand version usinhg part 2 rules
+func gen_sort_value_part2(s string) (r string) {
+
+	// generate all possible hands using jokers
+	// then sort the list of Joker-ized hands and select the best hand (last)
+	list := "23456789TQKA"
+	possible := []string{}
+	for n := 0; n < len(list); n++ {
+		possible = append(possible, handtype(strings.Replace(s, "J", list[n:n+1], -1)))
+		//pinfo(s, strings.Replace(s, "J", list[n:n+1], -1), handtype(strings.Replace(s, "J", list[n:n+1], -1)))
 	}
-	p := []int{}
-	for _, v := range m {
-		p = append(p, v)
-	}
-	sort.Ints(p[:])
-	sort.Sort(sort.Reverse(sort.IntSlice(p)))
-	p = append(p, 0, 0, 0, 0)
-	for _, v := range p {
-		r = r + strconv.Itoa(v)
-	}
-	r = r[:5]
+	sort.Slice(possible, func(i, j int) bool { return possible[i] < possible[j] })
+	r = possible[len(possible)-1] + s
 
 	// change card letters to be values that sort based on value
 	// this will let
-	// change card letters to be values that sort based on value
-	// this will let
-	s = strings.Replace(s, "A", "E", -1)
-	s = strings.Replace(s, "K", "D", -1)
-	s = strings.Replace(s, "Q", "C", -1)
-	s = strings.Replace(s, "J", "B", -1)
-	s = strings.Replace(s, "T", "A", -1)
-	r = r + "-" + s
+	r = translate(r, "TJQKA", "A1CDE")
+
 	return
 }
 
-func gen_sort_value_part2(s string) (r string) {
-	r = ""
-
-	// generate all possible hands using jokers
-	list := "23456789TQK"
-	possible := []string{}
-	for n := 0; n < len(list); n++ {
-		possible = append(possible, strings.Replace(s, "J", list[n:n+1], -1))
+// basically dirty hack of unix 'tr' command
+func translate(s, from, to string) string {
+	result := ""
+	for n := 0; n < len(s); n++ {
+		found := false
+		for j := 0; j < len(from); j++ {
+			if s[n] == from[j] {
+				found = true
+				result += to[j : j+1]
+			}
+		}
+		if !found {
+			result += s[n : n+1]
+		}
 	}
-	//pinfo(s, possible)
-	sortpossible := []string{}
-	for _, pos := range possible {
-		newpos := ""
-		m := map[string]int{}
-		for n := 0; n < len(pos); n++ {
-			m[pos[n:n+1]]++
-		}
-		q := []int{}
-		for _, v := range m {
-			q = append(q, v)
-		}
-		sort.Sort(sort.Reverse(sort.IntSlice(q)))
-		q = append(q, 0, 0, 0, 0)
-		for _, v := range q {
-			newpos = newpos + strconv.Itoa(v)
-		}
-		newpos = newpos[:5]
-		sortpossible = append(sortpossible, newpos)
+	return result
+}
+
+// returns the hand type based on hand provided
+func handtype(hand string) string {
+
+	m := map[string]int{}
+	q := []int{0, 0, 0, 0}
+	for n := 0; n < len(hand); n++ {
+		m[hand[n:n+1]]++
 	}
-	sort.Slice(sortpossible, func(i, j int) bool { return sortpossible[i] < sortpossible[j] })
-	r = sortpossible[len(sortpossible)-1]
-	//pinfo(r)
+	for _, v := range m {
+		q = append(q, v)
+	}
 
-	// change card letters to be values that sort based on value
-	// this will let
-	s = strings.Replace(s, "A", "E", -1)
-	s = strings.Replace(s, "K", "D", -1)
-	s = strings.Replace(s, "Q", "C", -1)
-	s = strings.Replace(s, "J", "1", -1)
-	s = strings.Replace(s, "T", "A", -1)
-
-	return
+	sort.Sort(sort.Reverse(sort.IntSlice(q)))
+	switch {
+	case q[0] == 5:
+		return "7"
+	case q[0] == 4:
+		return "6"
+	case q[0] == 3 && q[1] == 2:
+		return "5"
+	case q[0] == 3 && q[1] == 1:
+		return "4"
+	case q[0] == 2 && q[1] == 2:
+		return "3"
+	case q[0] == 2 && q[1] == 1:
+		return "2"
+	default:
+		return "1"
+	}
 }
 
 // returns input as eitrhegr from standard input or uses first
