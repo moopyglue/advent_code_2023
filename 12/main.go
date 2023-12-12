e7e564bc49e2c12ed805b801630b9fb8a49f78d8
Simon Wall <moopyglue@gmail.com>
2023-12-12 16:54:14 +0000

    about to begiin rewrite of 12 AGAIN

--- 12/main.go
+++ 12/main.go
@@ -6,6 +6,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -16,9 +17,19 @@ type row struct {
 	known string
 	pat   []int
 	diff  int
+	whole bool
 }
 
+var broken_string = " "
+var working_string = "O"
+
 func main() {
+
+	// create padded sttrings for ease of use later
+	for n := 0; n < 12; n++ {
+		broken_string += broken_string
+		working_string += working_string
+	}
 
 	fives := true
 	pinfo("starting")
@@ -42,86 +53,56 @@ func main() {
 			s = s + int(i)
 		}
 		s += len(p) - 1
-		rows = append(rows, row{known: a[0], pat: p, diff: len(a[0]) - s})
+		rows = append(rows, row{known: a[0], pat: p, diff: len(a[0]) - s, whole: true})
 	}
 
 	// for each row calculate the cost
 	part1res := 0
-	for _, r := range rows {
-		pinfo(r.known, r.pat, r.minpat, len(r.pat)+1, len(r.known)-r.minpat)
-		pinfo(len(options_list(len(r.known)-r.minpat, len(r.pat)+1)))
-		// re := regexp.MustCompile(r.known)
-		// for _, s := range make_solutions(r) {
-		// 	if re.MatchString(s) {
-		// 		//pinfo("--->", s)
-		// 		part1res++
-		// 	}
-		// }
+	for n := 0; n < len(rows); n++ {
+		pinfo(n, rows[n])
+		count := count_matches(rows[n])
+		part1res += count
+		pinfo(count, part1res)
 	}
 
 	pinfo("part1", part1res)
-
 }
 
-// make all the valid solutions for a specific row
-// uses options_list and then converts to string representation
-func make_solutions(r row) (result []string) {
-	type kk struct {
-		c int
-		s string
-	}
-	diff := len(r.known) - r.minpat
-	for _, x := range options_list(diff, len(r.pat)+1) {
-		tmp := []int{0}
-		for _, y := range r.pat {
-			tmp = append(tmp, y)
-			tmp = append(tmp, 1)
-		}
-		tmp = append(tmp[:len(tmp)-1], 0)
-		for k, v := range x {
-			tmp[k*2] = tmp[k*2] + v
-		}
-		res := ""
-		for n := 0; n < (len(tmp) - 2); n = n + 2 {
-			for x := 0; x < tmp[n]; x++ {
-				res += " "
-			}
-			for x := 0; x < tmp[n+1]; x++ {
-				res += "O"
-			}
-		}
-		for x := 0; x < tmp[len(tmp)-1]; x++ {
-			res += " "
-		}
-		result = append(result, res)
-	}
-	return
-}
+var cache = map[string]bool{}
 
-// recursive routine to calculate all the perms and combinations
-// when dividing a number of balls into a number of cups
-func options_list(balls, cups int) (list [][]int) {
-	if cups == 1 {
-		list = [][]int{{balls}}
+func count_matches(r row) (count int) {
+	count = 0
+	mod := 1
+	if r.whole {
+		mod = 0
+		r.whole = false
+	}
+	if len(r.pat) == 0 {
+		re := regexp.MustCompile(r.known)
+		if re.MatchString(broken_string[:len(r.known)]) {
+			count++
+		}
 	} else {
-		for b := 0; b <= balls; b++ {
-			k := options_list(balls-b, cups-1)
-			for _, x := range k {
-				list = append(list, []int{b})
-				for _, y := range x {
-					list[len(list)-1] = append(list[len(list)-1], y)
+		for n := 0; n <= r.diff; n++ {
+			s := broken_string[:n+mod] + working_string[:r.pat[0]]
+			tt := s + r.known[:len(s)]
+			if qq, ok := cache[tt]; ok {
+				if qq {
+					count += count_matches(row{known: r.known[len(s):], pat: r.pat[1:], diff: r.diff - n})
+				}
+			} else {
+				re := regexp.MustCompile(r.known[:len(s)])
+				if re.MatchString(s) {
+					cache[tt] = true
+					count += count_matches(row{known: r.known[len(s):], pat: r.pat[1:], diff: r.diff - n})
+				} else {
+					cache[tt] = false
 				}
 			}
 		}
+		r.whole = false
 	}
 	return
-}
-
-func abs(i int) int {
-	if i < 0 {
-		return -i
-	}
-	return i
 }
 
 // returns input as eitrhegr from standard input or uses first
@@ -148,13 +129,6 @@ func getlines() (lines []string) {
 	}
 	return
 
-}
-
-// quick and dirty single value response
-// string -> int64 conversion
-func i64(s string) (i int64) {
-	i, _ = strconv.ParseInt(s, 10, 0)
-	return
 }
 
 // debug printing for INFO style lines
